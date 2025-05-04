a4217330a880743df8c41a2b635685165e2b3fbc
Diogo França CGI <DiogoFranca.CGI@edp.com>
2025-05-04 23:21:43 +0100

    App Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Importar as páginas
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
 import AdminRegister from './pages/AdminRegister';
 import AdminPainel from './pages/AdminPainel';
+import Agenda from './pages/Agenda';
+import CadastroTurno from './pages/CadastroTurno';
+import RankingPresencas from './pages/RankingPresencas';
 
 function App() {
   return (
@@ -16,6 +20,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/admin/register" element={<AdminRegister />} />
         <Route path="/admin/painel" element={<AdminPainel />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/cadastrar" element={<CadastroTurno />} />
+        <Route path="/ranking" element={<RankingPresencas />} />
       </Routes>
     </Router>
   );
