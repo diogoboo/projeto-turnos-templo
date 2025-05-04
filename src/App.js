2cd54248406b4da5e2815e5dea017ba7bcbbb92b
Diogo França CGI <DiogoFranca.CGI@edp.com>
2025-05-04 21:53:51 +0100

    App Route for login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
